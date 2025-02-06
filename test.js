7fd96bc278fa6db4c3e5ad4d10fbd4abbc1532d0
lyjoice <lahely.na@gmail.com>
2025-02-06 19:55:48 +0100

    index.js lastname in appbar

---
+++ test.js
@@ -0,0 +1,26 @@
+/**
+ * cette function affiche username dans le span, le nom utilisateur du localStorage
+ * @param {*} username 
+ */
+
+function showUserMenu(username){
+    const usernameDisplay= document.getElementById("username-display")
+    usernameDisplay.textContent= username
+}
+//Unefois le DOM chargé, la fonction récupère l'username ()=>
+document.addEventListener("DOMContentLoaded", function(){
+    const storedUsername= localStorage.getItem("username")
+    const isAuthenticated= localStorage.getItem("isAuthenticated")
+    if(storedUsername && isAuthenticated === "true"){
+        const usernameDisplay= document.getElementById("username-display")
+        usernameDisplay.textContent= storedUsername 
+        showUserMenu(storedUsername)
+    }else{
+        window.location.href="login.html"
+    }
+})
+
+document.getElementById("logout-btn").addEventListener("click", function(){
+    localStorage.setItem("isAuthenticated",false)
+    window.location.href="login.html"
+})
