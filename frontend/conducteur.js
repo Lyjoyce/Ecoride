78e920f6756754fb8d6d3c7e53c932b5b0078c0a
lyjoice <lahely.na@gmail.com>
2025-03-25 21:55:12 +0100

    add js code comment in conductor.js

--- frontend/conducteur.js
+++ frontend/conducteur.js
@@ -20,3 +20,21 @@ document.getElementById("logout-btn").addEventListener("click", function(){
     localStorage.setItem("isAuthenticated",false)
     window.location.href="login.html"
 })
+
+/*
+const monImage = document.querySelector("img");
+
+let maRequete = new Request("fleurs.jpg");
+
+fetch(maRequete)
+  .then(function (reponse) {
+    if (!reponse.ok) {
+      throw new Error(`erreur HTTP! statut: ${reponse.status}`);
+    }
+    return reponse.blob();
+  })
+  .then(function (reponse) {
+    let URLobjet = URL.createObjectURL(reponse);
+    monImage.src = URLobjet;
+  });
+  */
