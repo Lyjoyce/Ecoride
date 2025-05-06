9b4aa9c5c9f25e5c6b7b329d9cda77bb50b5f910
lyjoice <lahely.na@gmail.com>
2025-05-06 17:41:25 +0200

    ecoride_frontend rename and clean

--- backend/ecorideApi/src/main/java/com/example/api/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.api.entities.Role;
-
-public interface RoleRepository extends JpaRepository<Role,Long>{
-
-}
--- backend/ecorideApi/src/main/java/com/example/api/repositories/VoitureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.api.entities.Voiture;
-
-public interface VoitureRepository extends JpaRepository<Voiture,Long> {
-
-}
